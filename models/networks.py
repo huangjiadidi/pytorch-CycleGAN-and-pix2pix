c6f770530345f118f531aa95de98e4aea588e38f
Jiajun Gary Huang <jhua7177@uni.sydney.edu.au>
2021-08-03 00:12:56 +1000

    1

--- models/networks.py
+++ models/networks.py
@@ -638,15 +638,13 @@ class Discriminator(nn.Module):
             nn.LeakyReLU(),
             nn.Conv2d(2 * DIM, 4 * DIM, 3, 2, padding=1),
             nn.LeakyReLU(),
-            nn.Conv2d(4 * DIM, 8 * DIM, 3, 2, padding=1),
-            nn.LeakyReLU(),
         )
 
         self.main = main
-        self.linear = nn.Linear(4*4*4*4*DIM, 1)
+        self.linear = nn.Linear(4*4*4*DIM, 1)
 
     def forward(self, input):
         output = self.main(input)
-        output = output.view(-1, 4*4*4*4*self.DIM)
+        output = output.view(-1, 4*4*4*DIM)
         output = self.linear(output)
         return output
